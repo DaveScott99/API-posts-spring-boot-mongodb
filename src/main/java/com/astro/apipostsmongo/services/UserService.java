d6997eae87ab599c6b3cb423bec3368d3fd0a232
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-16 15:09:50 -0300

    Obtendo um usuário por id

--- src/main/java/com/astro/apipostsmongo/services/UserService.java
+++ src/main/java/com/astro/apipostsmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.astro.apipostsmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.astro.apipostsmongo.domain.User;
 import com.astro.apipostsmongo.repository.UserRepository;
+import com.astro.apipostsmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 	
 }
