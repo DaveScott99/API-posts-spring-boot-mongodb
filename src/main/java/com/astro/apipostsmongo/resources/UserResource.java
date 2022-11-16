c04c781a07676c341f962d6f916fa64a1373f2da
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-16 15:20:14 -0300

    Inserção de usuário com POST

--- src/main/java/com/astro/apipostsmongo/resources/UserResource.java
+++ src/main/java/com/astro/apipostsmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.astro.apipostsmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.astro.apipostsmongo.domain.User;
 import com.astro.apipostsmongo.dto.UserDTO;
@@ -30,11 +34,18 @@ public class UserResource {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findByID(@PathVariable String id){
-		
 		User obj = userService.findById(id);
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
 		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/astro/apipostsmongo/services/UserService.java
+++ src/main/java/com/astro/apipostsmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.astro.apipostsmongo.domain.User;
+import com.astro.apipostsmongo.dto.UserDTO;
 import com.astro.apipostsmongo.repository.UserRepository;
 import com.astro.apipostsmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
