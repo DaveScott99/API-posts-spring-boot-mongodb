2ca91682bd2676cd0f4d424a2f8fdbcef1ca350c
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-16 12:33:24 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/astro/apipostsmongo/ApipostsmongoApplication.java
@@ -0,0 +1,13 @@
+package com.astro.apipostsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipostsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipostsmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astro/apipostsmongo/ApipostsmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.apipostsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipostsmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
