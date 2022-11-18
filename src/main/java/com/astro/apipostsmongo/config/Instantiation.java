1fdd853abefbbe455f1e4f82df8a6d99f4e591f9
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-18 10:48:21 -0300

    Referenciando os posts do usuário

--- src/main/java/com/astro/apipostsmongo/config/Instantiation.java
+++ src/main/java/com/astro/apipostsmongo/config/Instantiation.java
@@ -47,6 +47,9 @@ public class Instantiation implements CommandLineRunner {
 	
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
